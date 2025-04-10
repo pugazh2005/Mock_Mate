a51dda9db1357469435592c6fc9b2bc73a0728a3
pugazh2005 <spugazhendhi0@gmail.com>
2025-04-10 21:32:29 +0530

    resolved

--- app/api/vapi/generate/route.ts
+++ app/api/vapi/generate/route.ts
@@ -1,15 +1,15 @@
 import { generateText } from "ai";
 import { google } from "@ai-sdk/google";
+import 'dotenv/config';
 
 import { db } from "@/firebase/admin";
 import { getRandomInterviewCover } from "@/lib/utils";
 
 export async function POST(request: Request) {
   const { type, role, level, techstack, amount, userid } = await request.json();
-
   try {
     const { text: questions } = await generateText({
-      model: google("gemini-2.0-flash-001"),
+      model: google('gemini-2.0-flash-001'),
       prompt: `Prepare questions for a job interview.
         The job role is ${role}.
         The job experience level is ${level}.
@@ -24,6 +24,7 @@ export async function POST(request: Request) {
         Thank you! <3
     `,
     });
+    console.log("pugazhhhhhh",userid);
 
     const interview = {
       role: role,
@@ -36,8 +37,8 @@ export async function POST(request: Request) {
       coverImage: getRandomInterviewCover(),
       createdAt: new Date().toISOString(),
     };
-
     await db.collection("interviews").add(interview);
+    console.log("kjhgfdhygtfruhygtrhgfuhygfdhgfdhygfdygtfygtf",interview);
 
     return Response.json({ success: true }, { status: 200 });
   } catch (error) {
